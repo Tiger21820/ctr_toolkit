0463a1539570e1adc909557e203860fa4e81ecdc
ps3hen <devguy@hotmail.com.au>
2013-03-25 09:16:03 +0800

    Added copyright, and licensing

--- make_banner/yaml.h
+++ make_banner/yaml.h
@@ -4,6 +4,21 @@ YAML Interpeter
 
 Author: 3DSGuy © 2013
 
+This file is part of make_banner.
+
+make_banner is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+make_banner is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with make_banner.  If not, see <http://www.gnu.org/licenses/>.
+
 ********************************************************/
 
 
