af0c949d9d19c3e7a0a95b2ed3185289ae00c3e9
ps3hen <devguy@hotmail.com.au>
2013-04-03 23:27:54 +0800

    fixes for linux

--- extdata_tool/ExtData.c
+++ extdata_tool/ExtData.c
@@ -3,18 +3,18 @@ Copyright 2013 3DSGuy
 
 This file is part of extdata_tool.
 
-make_banner is free software: you can redistribute it and/or modify
+extdata_tool is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-make_banner is distributed in the hope that it will be useful,
+extdata_tool is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with make_banner.  If not, see <http://www.gnu.org/licenses/>.
+along with extdata_tool.  If not, see <http://www.gnu.org/licenses/>.
 **/
 #include "lib.h"
 #include "ExtData.h"
